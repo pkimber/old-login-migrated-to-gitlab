1fb0ef51b6f994e210eee70c63af1bccee8f578d
Patrick Kimber <code@pkimber.net>
2013-09-09 19:55:13 +0100

    Refactor tests - try and use a standard scenario

---
+++ login/tests/scenario.py
@@ -0,0 +1,30 @@
+from django.contrib.auth.models import User
+
+from login.tests.model_maker import (
+    make_superuser,
+    make_user,
+)
+
+
+def get_fred():
+    return User.objects.get(username='fred')
+
+
+def get_sara():
+    return User.objects.get(username='sara')
+
+
+def user_default():
+    # superuser
+    make_superuser('admin')
+    # member of staff
+    make_user('staff', is_staff=True)
+    # standard user with no extra permissions
+    make_user('web')
+
+
+def user_contractor():
+    # fred is a farmer
+    make_user('fred')
+    # steph is a smallholder
+    make_user('sara')
