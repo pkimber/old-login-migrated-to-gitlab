bfc34612069ba987f9e675a550fd16ece9446cdd
Patrick Kimber <code@pkimber.net>
2014-06-02 18:57:41 +0100

    version 0.1.06

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup(
             'templates/registration/*.*',
         ],
     },
-    version='0.1.05',
+    version='0.1.06',
     description='User Login',
     author='Patrick Kimber',
     author_email='code@pkimber.net',
