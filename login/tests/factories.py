778d0f7b0055220322558de2d16d6c9ecd53b16c
Patrick Kimber <code@pkimber.net>
2014-07-08 18:34:02 +0100

    factory update

--- login/tests/factories.py
+++ login/tests/factories.py
@@ -11,4 +11,15 @@ class UserFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = User
 
-    username = factory.Sequence(lambda n: 'user_{:02d}'.format(n))
+    is_active = True
+    is_staff = False,
+    is_superuser = False
+    password = factory.PostGenerationMethodCall('set_password', 'pass')
+
+    @factory.sequence
+    def first_name(n):
+        return 'first_name_{:02d}'.format(n)
+
+    @factory.sequence
+    def username(n):
+        return 'user_{:02d}'.format(n)
