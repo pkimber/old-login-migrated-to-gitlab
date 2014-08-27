167058808ab9e11213000d64bb9dec1fb7f55c47
Patrick Kimber <code@pkimber.net>
2014-08-27 23:33:19 +0100

    version 0.1.21

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
             'templates/login/*.*',
         ],
     },
-    version='0.1.20',
+    version='0.1.21',
     description='User Login',
     author='Patrick Kimber',
     author_email='code@pkimber.net',
